748f14718d4361b6e12d970958daa3aea2c19464
Ryan Chan Shum Hang <rchanshu@scs.ryerson.ca>
2019-01-17 17:32:30 -0500

    updated board.java

--- Board.java
+++ Board.java
@@ -1,5 +1,42 @@
 class Board{
+  static final int DefaultTowers = 3;
+
+  private int numDiscs;
+  private Tower[] towers;
+
   public Board(){
+    this(DefaultTowers);
+  }
+
+  public Board(int numDiscs){
+    this(numDiscs, DefaultTowers);
+  }
+
+  public Board(int numDiscs, int numTowers){
+    towers = new Tower[numTowers];
+    this.numDiscs = numDiscs;
+
+    for(int i = 0; i < towers.length; i++){
+      towers[i] = new Tower(numDiscs);
+    }
+
+    for(int i = numDiscs; i > 0; i--){
+      towers[0].add(new Disc(i));
+    }
+  }
+
+  public String toString(){
+    StringBuilder s = new StringBuilder();
+
+    for(int j = 0; j < towers[0].size(); j++){
+      for(int i = 0; i < towers.length; i++){
+        s.append(towers[i].getDisc(j));
+        s.append("\t");
+      }
+      s.append("\n");
+    }
+
+    return s.toString();
   }
 
   public void solve(){
